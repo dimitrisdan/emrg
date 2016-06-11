6d6fff37fb33a9c56ceafed0449615cb31d11b03
dimitris <dimitris.danampasis@gmail.com>
2016-06-11 13:46:35 +0200

    patient

--- database/migrations/2016_06_05_013450_create_patients_table.php
+++ database/migrations/2016_06_05_013450_create_patients_table.php
@@ -34,48 +34,45 @@ class CreatePatientsTable extends Migration
             $table->foreign('contact_id')->references('contact_id')->on('contacts');
         });
 
-//        DB::table('patients')->insert(
-//            array(
-//                'patient_id' => 1,
-//                'contact_id' => 1,
-//                'guardian_id' => 1,
-//                'user_id' => 1,
-//                'patient_nationalid' => rand(100000000000,999999999999),
-//                'patient_firstname' => 'Dimitris',
-//                'patient_lastname' => 'Danampasis',
-//                'patient_dob' => '02/04/1986',
-//                'patient_gender' => 'Male',
-//                'patient_insurance' => 'AB666666'
-//            )
-//        );
-//        DB::table('patients')->insert(
-//            array(
-//                'patient_id' => 2,
-//                'contact_id' => 2,
-//                'guardian_id' => 2,
-//                'user_id' => 2,
-//                'patient_nationalid' => rand(100000000000,999999999999),
-//                'patient_firstname' => 'Olga',
-//                'patient_lastname' => 'Triantou',
-//                'patient_dob' => '02/04/1956',
-//                'patient_gender' => 'Female',
-//                'patient_insurance' => 'AB555555'
-//            )
-//        );
-//        DB::table('patients')->insert(
-//            array(
-//                'patient_id' => 3,
-//                'contact_id' => 3 ,
-//                'guardian_id' => 3,
-//                'user_id' => 3,
-//                'patient_nationalid' => rand(100000000000,999999999999),
-//                'patient_firstname' => 'Tina',
-//                'patient_lastname' => 'Mpitouni',
-//                'patient_Dob' => '02/04/1986',
-//                'patient_gender' => 'Female',
-//                'patient_insurance' => 'AB222222'
-//            )
-//        );
+        DB::table('patients')->insert(
+            array(
+                'contact_id' => 1,
+                'guardian_id' => 1,
+                'user_id' => 1,
+                'patient_nationalid' => rand(100000000000,999999999999),
+                'patient_firstname' => 'Dimitris',
+                'patient_lastname' => 'Danampasis',
+                'patient_dob' => '02/04/1986',
+                'patient_gender' => 'Male',
+                'patient_insurance' => 'AB666666'
+            )
+        );
+        DB::table('patients')->insert(
+            array(
+                'contact_id' => 2,
+                'guardian_id' => 2,
+                'user_id' => 2,
+                'patient_nationalid' => rand(100000000000,999999999999),
+                'patient_firstname' => 'Olga',
+                'patient_lastname' => 'Triantou',
+                'patient_dob' => '02/04/1956',
+                'patient_gender' => 'Female',
+                'patient_insurance' => 'AB555555'
+            )
+        );
+        DB::table('patients')->insert(
+            array(
+                'contact_id' => 3 ,
+                'guardian_id' => 3,
+                'user_id' => 3,
+                'patient_nationalid' => rand(100000000000,999999999999),
+                'patient_firstname' => 'Tina',
+                'patient_lastname' => 'Mpitouni',
+                'patient_Dob' => '02/04/1986',
+                'patient_gender' => 'Female',
+                'patient_insurance' => 'AB222222'
+            )
+        );
     }
 
     /**
