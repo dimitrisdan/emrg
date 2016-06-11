a8295fc32e8c6aa4ba53f69879ab2513116d74ca
dimitris <dimitris.danampasis@gmail.com>
2016-06-11 15:11:33 +0200

    patient

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -18,41 +18,53 @@ class PatientController extends Controller
     private function createPatient()
     {
         $patient = Patient::create();
+        $contact = Contact::create();
+
         return $patient;
     }
+
+    public function patientExists($user_id)
+    {
+        $patient = Patient::where('user_id', '=', $user_id)->get()->first();
+        if($patient)
+            return true;
+        else
+            return false;
+    }
+
     private function getContact($patient){
-        //return Contact::find($patient->contact_id);
         return Contact::find($patient->contact_id);
     }
+
     public function getDashboard(Request $request)
     {
         $user_id = $request->user()->id;
 
-
-        $patient = Patient::where('user_id', '=', $user_id)->get()->first();
-        if(!$patient)
+        if(!patientExists($user_id))
         {
-            $patient =$this->createPatient();
-        }
-        $contact = $this->getContact($patient);
-        $guardian = Patient::find($user_id)->guardian()->get()->first();
-        $allergies = Patient::find($user_id)->allergies()->get();
-        $medicals = Patient::find($user_id)->medicalAlerts()->get();
-        
-        $agents = array();
-        foreach ($allergies as $allergy){
-            $agent = Allergy::find($allergy->allergy_id)->allergyagent()->get()->first();
-            array_push($agents,$agent);
+            echo 'patient doesn\'t exist';
+//            $patient =$this->createPatient();
         }
 
-        return view('dashboard', [
-            'email' => $request->user()->email,
-            'patient' => $patient,
-            'contact' => $contact,
-            'guardian' => $guardian,
-            'allergies' => $allergies,
-            'agents' => $agents,
-            'medicals' => $medicals
-        ]);
+//        $contact = $this->getContact($patient);
+//        $guardian = Patient::find($user_id)->guardian()->get()->first();
+//        $allergies = Patient::find($user_id)->allergies()->get();
+//        $medicals = Patient::find($user_id)->medicalAlerts()->get();
+//
+//        $agents = array();
+//        foreach ($allergies as $allergy){
+//            $agent = Allergy::find($allergy->allergy_id)->allergyagent()->get()->first();
+//            array_push($agents,$agent);
+//        }
+
+//        return view('dashboard', [
+//            'email' => $request->user()->email,
+//            'patient' => $patient,
+//            'contact' => $contact,
+//            'guardian' => $guardian,
+//            'allergies' => $allergies,
+//            'agents' => $agents,
+//            'medicals' => $medicals
+//        ]);
     }
 }
